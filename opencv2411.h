47a1e48f6be98ab839b0eb805eed655d2607739f
JunyaYoshida <yoshidajunya@gmail.com>
2015-06-10 20:35:34 +0900

    opencv version

--- opencv2411.h
+++ opencv2411.h
@@ -3,33 +3,25 @@
 #ifdef _WIN64
 
 #ifdef _DEBUG
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_core2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_imgproc2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_highgui2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_ml2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_objdetect2411d.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc12\\lib\\opencv_core2411d.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc12\\lib\\opencv_imgproc2411d.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc12\\lib\\opencv_highgui2411d.lib")
 #else
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_core2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_imgproc2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_highgui2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_ml2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc10\\lib\\opencv_objdetect2411.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc12\\lib\\opencv_core2411.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc12\\lib\\opencv_imgproc2411.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x64\\vc12\\lib\\opencv_highgui2411.lib")
 #endif
 
 #else
 
 #ifdef _DEBUG
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_core2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_imgproc2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_highgui2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_ml2411d.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_objdetect2411d.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc12\\lib\\opencv_core2411d.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc12\\lib\\opencv_imgproc2411d.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc12\\lib\\opencv_highgui2411d.lib")
 #else
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_core2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_imgproc2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_highgui2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_ml2411.lib")
-	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc10\\lib\\opencv_objdetect2411.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc12\\lib\\opencv_core2411.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc12\\lib\\opencv_imgproc2411.lib")
+	#pragma comment(lib,"C:\\opencv\\opencv2411\\build\\x86\\vc12\\lib\\opencv_highgui2411.lib")
 #endif
 
 #endif
